03b6af7da3f379f9ebb69d721bdfce6056ec9d5f
KJ Krueger <kjkrueger12@gmail.com>
2022-10-12 12:20:02 -0700

    debugged

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -45,7 +45,7 @@ module.exports = () => {
         ],
       }),
     ],
-    ],
+    
 
     module: {
       rules: [
@@ -69,18 +69,5 @@ module.exports = () => {
     },
   };
 };
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
+
 
